67a599d4c6e1bded85509d827c2fd0b52a809f5a
Zaman <RC0025635@student.riversidecollege.ac.uk>
2018-06-20 09:32:50 +0100

    Add project files.

---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        class row
+        {
+            double time;
+            double charged;
+        }
+
+        List<row> table = new List<row>();
+        public Form1()
+        {
+
+                
+            InitializeComponent();
+        }
+
+        private void openToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+
+     
+
+        }
+    }
+}
